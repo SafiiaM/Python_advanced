29676be9d0d4e8e4590210aa64aa8dd33d799a5f
SafiiaM <107404606+SafiiaM@users.noreply.github.com>
2023-09-10 09:25:23 +0300

    changed task_2 HW_4

--- seminar_3_hw/seminar_4_hw/task_2.py
+++ seminar_3_hw/seminar_4_hw/task_2.py
@@ -12,3 +12,30 @@ def reverse_key(**kwargs):
 
 
 print(reverse_key(std=[100, 300, -500], skb=[150, 600, -700], srd=[400, 400, -400]))
+
+# {'[100, 300, -500]': 'std', '[150, 600, -700]': 'skb', '[400, 400, -400]': 'srd'}
+
+# решение преподавателя:
+
+def inverse_try(**kwargs) -> dict:
+    new_dict={}
+    for key,value in kwargs.items():
+       try:
+           new_dict[value]=key
+       except:
+           new_dict[str(value)]=key
+    return new_dict
+
+def inverse_instance(**kwargs) -> dict:
+    new_dict={}
+    for key,value in kwargs.items():
+       if not isinstance(value, list|set|dict):
+           new_dict[value]=key
+       else:
+           new_dict[str(value)]=key
+    return new_dict
+print(inverse_try(one=1, two=['2'], three=(3,)))
+print(inverse_instance(one=1, two=['2'], three=(3,)))
+
+# {1: 'one', "['2']": 'two', (3,): 'three'}
+# {1: 'one', "['2']": 'two', (3,): 'three'}
